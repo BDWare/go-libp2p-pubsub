5d84b8240a832b5b7d5cf03bd9cdf4fd562fef69
huyingcong <huyingcong@internetapi.cn>
2020-12-02 09:42:59 +0800

    move option in randomsub to bdwopt

--- randomsub.go
+++ randomsub.go
@@ -6,10 +6,8 @@ package pubsub
 
 import (
 	"context"
-	"fmt"
 	"math"
 
-	pb "github.com/bdware/go-libp2p-pubsub/pb"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/protocol"
@@ -184,48 +182,4 @@ func (rs *RandomSubRouter) Join(topic string) {
 
 func (rs *RandomSubRouter) Leave(topic string) {
 	rs.tracer.Join(topic)
-}
-
-/* BDWare */
-
-// RandomSubDGenerator is the function that controls how many peers to be forwarded.
-type RandomSubDGenerator func(msg *pb.Message) int
-
-// DefaultRandomSubDGenerator returns the default RandomSubD
-func DefaultRandomSubDGenerator(msg *pb.Message) int {
-	return RandomSubD
-}
-
-// WithRandomSubDGenerator is the option that changes randomSubDGenerator
-func WithRandomSubDGenerator(gen RandomSubDGenerator) Option {
-	return func(p *PubSub) error {
-		rt, ok := p.rt.(*RandomSubRouter)
-		// check rt's type
-		if !ok {
-			return fmt.Errorf("unexpected router type: need to be RandomSub")
-		}
-		if rt.gen == nil {
-			return fmt.Errorf("unexpected nil generator")
-		}
-		// change rt's generator
-		rt.gen = gen
-		return nil
-	}
-}
-
-// WithCustomProtocols changes the protocols of randomsub.
-func WithCustomProtocols(protos []protocol.ID) Option {
-	return func(p *PubSub) error {
-		rt, ok := p.rt.(*RandomSubRouter)
-		// check rt's type
-		if !ok {
-			return fmt.Errorf("unexpected router type: need to be RandomSub")
-		}
-		if len(protos) == 0 {
-			return fmt.Errorf("unexpected empty protos")
-		}
-		// change rt's generator
-		rt.protocols = protos
-		return nil
-	}
 }
